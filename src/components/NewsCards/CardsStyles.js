586101fd76e093fffc0c11af86b5038ba15091d4
ashushreyansh <ashushreyansh@gmail.com>
2022-04-15 15:06:20 +0530

    newsCard(s) updated

--- src/components/NewsCards/CardsStyles.js
+++ src/components/NewsCards/CardsStyles.js
@@ -1,11 +1,23 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
-const styles = makeStyles({
+export default makeStyles({
     container: {
         padding: '0 50%',
         width:'100%',
         margin: 0,
-    }
-})
-
-export default styles;
+    },
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        height: '45vh',
+        padding: '10%',
+        borderRadius: 10,
+        color: 'white',
+      },
+    infoCard: {
+        display: 'flex', flexDirection: 'column', textAlign: 'center',
+    },
+});
