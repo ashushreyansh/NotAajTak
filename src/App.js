a9611994a972bc7f37a5b373ec4e348f87094c58
ritik002 <ritik.me@gmail.com>
2022-04-21 22:06:37 +0530

    Updated Code

--- src/App.js
+++ src/App.js
@@ -5,51 +5,54 @@ import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
 import wordsToNumbers from 'words-to-numbers';
 
-const alanKey = 'eba5f681a738efa7253f12eedb98f7ca2e956eca572e1d8b807a3e2338fdd0dc/stage';
+const alanKey =
+    'eba5f681a738efa7253f12eedb98f7ca2e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const [activeArticle, setActiveArticle] = useState(0);
-  const [newsArticles, setNewsArticles] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
+    useEffect(() => {
+        alanBtn({
+            key: 'eba5f681a738efa7253f12eedb98f7ca2e956eca572e1d8b807a3e2338fdd0dc/stage',
+            onCommand: ({ command, articles, number }) => {
+                if (command === 'newHeadlines') {
+                    console.log(articles);
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if (command === 'instructions') {
+                    setIsOpen(true);
+                } else if (command === 'highlight') {
+                    setActiveArticle(
+                        (prevActiveArticle) => prevActiveArticle + 1
+                    );
+                } else if (command === 'open') {
+                    const parsedNumber =
+                        number.length > 2
+                            ? wordsToNumbers(number, { fuzzy: true })
+                            : number;
+                    const article = articles[parsedNumber - 1];
 
-  useEffect(() => {
-    alanBtn({
-      key: 'eba5f681a738efa7253f12eedb98f7ca2e956eca572e1d8b807a3e2338fdd0dc/stage',
-      onCommand: ({ command, articles, number }) => {
-        if (command === 'newHeadlines') {
-          console.log(articles);
-          setNewsArticles(articles);
-          // setActiveArticle(-1);
-        // } else if (command === 'instructions') {
-        //   setIsOpen(true);
-        // } else if (command === 'highlight') {
-        //   setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
-        // } else if (command === 'open') {
-        //   const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
-        //   const article = articles[parsedNumber - 1];
+                    if (parsedNumber > articles.length) {
+                        alanBtn().playText('Please try that again...');
+                    } else if (article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...');
+                    } else {
+                        alanBtn().playText('Please try that again...');
+                    }
+                }
+            },
+        });
+    }, []);
 
-        //   if (parsedNumber > articles.length) {
-        //     alanBtn().playText('Please try that again...');
-        //   } else if (article) {
-        //     window.open(article.url, '_blank');
-        //     alanBtn().playText('Opening...');
-        //   } else {
-        //     alanBtn().playText('Please try that again...');
-        //   }
-        }
-      },
-    });
-  }, []);
-
-  return (
-    <div>
-      <h1>
-        Alan AI News App
-      </h1>
-      <NewsCards articles={newsArticles} />
-      <div class="alan-btn"></div>
-    </div>
-  );
-}
+    return (
+        <div>
+            <h1>Alan AI News App</h1>
+            <NewsCards articles={newsArticles} />
+            <div class='alan-btn'></div>
+        </div>
+    );
+};
 
 export default App;
